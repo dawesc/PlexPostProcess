393f7030715c4d18e4ffd795c5339f9e95bfd5ba
Christopher Dawes <christopher.dawes@camding.com>
2019-04-26 21:04:11 +0100

    Allow us to skip handbrake

--- com/camding/plexpostprocess/steps/DetermineFilename.py
+++ com/camding/plexpostprocess/steps/DetermineFilename.py
@@ -15,31 +15,31 @@ class DetermineFilename(object):
     self.__ltz    = pytz.timezone('Europe/London')    #For some reason plex writes it in pacific time on freenas
     self.__tmpDir = Settings.GetConfig('Paths', 'backup', '/mnt/PlexRecordings/BackupMP2')
     self.__plexLibraryPath = "/usr/local/plexdata-plexpass/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db"
- 
+
   def GetPlexLibraryPath(self):
     return self.__plexLibraryPath
-  
+
   def SetPlexLibraryPath(self, plexLibraryPath):
     self.__plexLibraryPath = plexLibraryPath
-  
+
   def GetPlexPostProcess(self):
     return self.__plexPostProcess;
-  
+
   def GetCorriePosibility(self, row):
     _epIx    = row[0]
     epTitle = row[1]
     epGuid  = row[2]
-    
+
     epSeries = row[2].replace('com.plexapp.agents.thetvdb://71565/', '')
     epSeries = epSeries[:epSeries.index('/')]
     epNr     = row[2].replace('com.plexapp.agents.thetvdb://71565/' + epSeries + '/', '')
-    epNr     = epNr[0:epNr.index('?')]  
+    epNr     = epNr[0:epNr.index('?')]
     epPart   = None
-    
+
     if "Part" in epTitle:
       epPartIx = epTitle.index("Part ") + 5
       epPart = int(epTitle[epPartIx:epPartIx + 1])
-    
+
     return [epGuid,epTitle,int(epSeries),int(epNr),epPart]
 
   def GetCorrieIndex(self, locCreateDt):
@@ -48,34 +48,35 @@ class DetermineFilename(object):
     c = conn.cursor()
     formattedDate = locCreateDt.strftime("%A,%%" + ordinal(locCreateDt.day) + " %B %Y")
     sql = "select \"index\",title,guid from metadata_items where library_section_id=17 and guid like '%/71565/%' and metadata_type = 4 and title like '%" + formattedDate + "%';"
-    
+
     possibilities = []
     for row in c.execute(sql):
       possibilities.append(self.GetCorriePosibility(row))
-    
+
     conn.close()
     if len(possibilities) == 0:
       return None
-      
+
     hasPart = False
     for posibility in possibilities:
       hasPart = hasPart or posibility[4] != None
-      
+
     if hasPart:
       desired = possibilities[0][4]
       if locCreateDt.hour == 19:
         desired = 1
       elif locCreateDt.hour == 20:
         desired = 2
-          
+
       if possibilities[0][4] != desired:
         possibilities[0][3] = possibilities[0][3] - possibilities[0][4] + desired
         possibilities[0][4] = desired
-    
+
     return possibilities[0]
 
   def GetNewCoronationStreetFilename(self, baseFile, _queuedFile):
-    baseFile = baseFile[0:baseFile.rfind('.')]
+    fileExtension = baseFile[baseFile.rfind('.') + 1:]
+    baseFile      = baseFile[0:baseFile.rfind('.')]
     if "Coronation Street" in baseFile:
       baseParts = baseFile.split(' - ')
       dtFormat = "%Y-%m-%d %H %M %S"
@@ -90,21 +91,22 @@ class DetermineFilename(object):
         epInfo = 's' + str(metaInfo[2]) + 'e' + str(metaInfo[3])
         baseParts.insert(1, epInfo)
       baseFile = ' - '.join(baseParts);
-   
-    return baseFile + ".mp4"
-    
+    if Settings.GetConfig("Applications", "handbrake", "false").lower in ['true', '1', 't', 'y', 'yes', 'yeah', 'yup', 'certainly', 'uh-huh', 'on' ]:
+      return baseFile + ".mp4"
+    else:
+      return baseFile + "." + fileExtension
+
   def GetDestFilename(self, queuedFile):
     baseFile = ntpath.basename(queuedFile.GetFilename())
     if queuedFile.GetFiletype() == 'm4v':
-      return os.path.join(os.path.dirname(os.path.abspath(queuedFile.GetFilename())), 
+      return os.path.join(os.path.dirname(os.path.abspath(queuedFile.GetFilename())),
                         self.GetNewCoronationStreetFilename(baseFile, queuedFile))
     else:
-      return os.path.join(os.path.dirname(os.path.abspath(queuedFile.GetFilename())), 
+      return os.path.join(os.path.dirname(os.path.abspath(queuedFile.GetFilename())),
                         baseFile[0:baseFile.rfind('.')] + ".mp3")
-  
+
   def GetTempFilename(self, queuedFile):
     if queuedFile.GetFiletype() == 'm4v':
       return os.path.join(self.__tmpDir, ntpath.basename(queuedFile.GetFilename()) + "." + str(queuedFile.GetId()) + ".mp4")
     else:
       return os.path.join(self.__tmpDir, ntpath.basename(queuedFile.GetFilename()) + "." + str(queuedFile.GetId()) + ".mp3")
-  
--- com/camding/plexpostprocess/steps/Transcode.py
+++ com/camding/plexpostprocess/steps/Transcode.py
@@ -14,21 +14,21 @@ from com.camding.plexpostprocess.Settings import Settings
 class Transcode(object):
   def __init__(self, plexPostProcess):
     self.__plexPostProcess = plexPostProcess
-  
+
   def GetPlexPostProcess(self):
     return self.__plexPostProcess;
-  
+
   def Transcode(self, _i, queuedFile):
     filenameHandler = DetermineFilename(self.GetPlexPostProcess())
     print("  PlexPostProcess to " + filenameHandler.GetTempFilename(queuedFile));
     self.GetPlexPostProcess().GetDatabaseInteraction().AddQFHistory(queuedFile, "Transcode", "  PlexPostProcess to " + filenameHandler.GetTempFilename(queuedFile));
-    
+
     command = []
-    
+
     if queuedFile.GetFiletype() == 'm4v':
       command = [
-        Settings.GetConfig('Applications', 'handbrake', '/usr/local/bin/HandBrakeCLI'), 
-        '--preset-import-file', 
+        Settings.GetConfig('Applications', 'handbrake', '/usr/local/bin/HandBrakeCLI'),
+        '--preset-import-file',
         os.path.join(Settings.GetRootPath(), 'handbrake_preset.json'),
         '-i',
         queuedFile.GetFilename(),
@@ -38,24 +38,34 @@ class Transcode(object):
         'Super HQ 1080p30 Surround MP3',
         '--decomb',
         'bob']
+      if Settings.GetConfig("Applications", "handbrake", "false").lower not in ['true', '1', 't', 'y', 'yes', 'yeah', 'yup', 'certainly', 'uh-huh', 'on' ]:
+        command = None
+
     else:
       command = [
-        Settings.GetConfig('Applications', 'ffmpeg', '/usr/local/bin/ffmpeg'), 
+        Settings.GetConfig('Applications', 'ffmpeg', '/usr/local/bin/ffmpeg'),
         '-i',
         queuedFile.GetFilename(),
         '-vn',
         '-acodec',
         'copy',
         filenameHandler.GetTempFilename(queuedFile)]
-  
+
+    if command is not None:
+      self.RunCommand(command)
+    else:
+      queuedFile.SetState(PlexPostProcessState.MOVING_FILES)
+      self.GetPlexPostProcess().GetDatabaseInteraction().UpdateQFState(queuedFile, "Transcode", "Transcode skipped")
+
+  def RunCommand(self, command):
     logfile_queue = Queue()
     logfile_queue.put(" ".join(command).encode('utf-8'))
     sys.stdout.write(" ".join(command))
     sys.stdout.flush()
-  
+
     # Launch the process with PIPE stdout and stderr
     process = self.GetPlexPostProcess().RunProcess(command, None, sys.stdin, PIPE, PIPE)
-  
+
     # Function for reader threads, echo lines from in_fh to out_fh and out_queue
     def read_handler(in_fh, out_fh, out_queue):
       while True:
@@ -64,34 +74,34 @@ class Transcode(object):
         out_fh.buffer.write(line)
         out_fh.flush()
         out_queue.put(line)
-  
+
     # Function for writer thread, echo lines from in_queue to out_fh
     def write_handler(in_queue, queuedFile, databaseInteraction):
       while True:
         line = in_queue.get()
         if line is None: return
         databaseInteraction.AddQFHistory(queuedFile, "Transcode", line)
-  
+
     # Launch a thread for reading stdout, reading stderr, and writing the logfile
     stdout_thread = Thread(target=read_handler, args=(process.stdout, sys.stdout, logfile_queue))
     stderr_thread = Thread(target=read_handler, args=(process.stderr, sys.stderr, logfile_queue))
     logfile_thread = Thread(target=write_handler, args=(logfile_queue, queuedFile, self.GetPlexPostProcess().GetDatabaseInteraction()))
-  
+
     for thread in stdout_thread, stderr_thread, logfile_thread:
       thread.daemon = True
       thread.start()
-  
+
     # Wait for the process to complete
     process.wait()
-  
+
     # Wait for stdout and stderr threads to complete
     for thread in stdout_thread, stderr_thread:
       thread.join()
-  
+
     # Signal and wait for the logfile thread to complete
     logfile_queue.put(None)
     logfile_thread.join()
-  
+
     if process.returncode == 0:
       if queuedFile.GetFiletype() == 'm4v':
         queuedFile.SetState(PlexPostProcessState.MOVING_FILES)
@@ -101,4 +111,3 @@ class Transcode(object):
     else:
       queuedFile.SetState(PlexPostProcessState.ERROR)
       self.GetPlexPostProcess().GetDatabaseInteraction().UpdateQFState(queuedFile, "Transcode", "Error " + str(process.returncode))
-      
